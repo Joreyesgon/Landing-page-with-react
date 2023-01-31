cc17118c8d57e0f6353f43aa89b39f05f6e47af6
Jose Reyes <jo.reyesgon@gmail.com>
2023-01-31 10:12:54 -0300

    minor changes

--- src/CardImage.js
+++ src/CardImage.js
@@ -6,8 +6,8 @@ const CardImage = () => {
             <div className="carousel-item active">
                 <img src="https://st3.depositphotos.com/12439124/15009/i/600/depositphotos_150090052-stock-photo-black-leather-texture-background.jpg" className="d-block w-100" alt="..." />
                 <div className="card-img-overlay">
-                    <p className="bg-light fs-1 text mt-5">One Page Wonder</p>
-                    <h3 className="card-title bg-light mt-3">Will Knock Your Socks Off</h3>
+                    <p className="bg-light display-1 text mt-5">One Page Wonder</p>
+                    <h3 className="bg-light display-4 mt-3">Will Knock Your Socks Off</h3>
     
                 </div>
             </div>
